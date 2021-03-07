7c6c3a864325a75ffaa1b13f8b4b4e333fec1776
cubasve <eva.cubasvasquez@gmail.com>
2021-03-07 00:48:59 -0500

    Add methods: newGameClick, pegClick, scoreClick

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -47,6 +47,69 @@ export default class App extends Component {
     this.setState({ selectedColorIndex: colorIndex });
   }
 
+  handleNewGameClick = () => {
+    this.setState(this.getInitialState());
+  }
+
+  handlePegClick = (pegIndex) => {
+    let currentGuessIndex = this.state.guesses.length - 1; //get index of last guess object
+    
+    //Replace objects/arrays with new ones
+    let guessesCopy = [...this.state.guesses];
+    let guessCopy = {...guessCopy[currentGuessIndex]};
+    let codeCopy = [...guessCopy.code];
+
+    codeCopy[pegIndex] = this.state.selectedColorIndex; 
+    //update new code array with the currently selected color
+
+    guessCopy.code = codeCopy; //update the new guess object
+
+    guessesCopy[currentGuessIndex] = guessCopy; //update new guesses array
+
+    this.setState({ guesses: guessesCopy });
+  }
+
+  handleScoreClick = () => {
+    //need index of current guess object(last object in guesses array)
+    let currentGuessIndex = this.state.guesses.length - 1;
+
+    let guessCodeCopy = [...this.state.guesses[currentGuessIndex].code];
+    let secretCodeCopy = [...this.state.code];
+
+    let perfect = 0, almost = 0;
+
+    guessCodeCopy.forEach((code, index) => {
+      if (secretCodeCopy[index] === code) {
+        perfect++;
+        guessCodeCopy[index] = secretCodeCopy[index] = null;
+      }
+    });
+
+    guessCodeCopy.forEach((code, index) => {
+      if (code === null) return;
+      let foundIndex = secretCodeCopy.indexOf(code);
+      if (foundIndex > -1) {
+        almost++;
+        secretCodeCopy[foundIndex] = null;
+      }
+    });
+
+    let guessesCopy = [...this.state.guesses];
+    let guessCopy = {...guessesCopy[currentGuessIndex]};
+    let scoreCopy = {...guessCopy.score};
+
+    scoreCopy.perfect = perfect;
+    scoreCopy.almost = almost;
+
+    guessCopy.score = scoreCopy;
+
+    guessesCopy[currentGuessIndex] = guessCopy;
+
+    if (perfect !== 4) guessCopy.push(this.getNewGuess());
+
+    this.setState({ guesses: guessesCopy });
+  }
+
   render() {
     let winTries = this.getWinTries();
 
